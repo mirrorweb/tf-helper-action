6e75b3afff9fbdf9dd9b0d17d67dd46f18bf38cc
Mark Johnson <mark.johnson@mirrorweb.com>
2020-04-18 20:21:35 +0100

    debug actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,22 @@ set -x
 
 readonly delimiter=":::"
 
-for arg in $(env | grep 'INPUT_');do
-    if [[ $arg == *$delimiter* ]];then
-        subcommand="$(echo $arg | sed 's/^.*INPUT_.*=//' | awk -F':::' '{print $1}')"
-        options="$(echo $arg | sed 's/^.*INPUT_.*=//' | awk -F':::' '{print $2}')"
-        if [[ ${subcommand} == "pushconfig" ]]; then
-            tfh ${subcommand} ${options}
-        elif [[ ${subcommand} == "pushvars" ]]; then
-             tfh ${subcommand} ${options}
-        elif [[ ${subcommand} == 'pullvars' ]]; then
-            tfh ${subcommand} ${options}
-        else
-            echo "tfh subcommand ${subcommand} not yet supported"
-        fi
-    fi
-done
+env | grep 'INPUT_'
+
+#for arg in $(env | grep 'INPUT_');do
+#    if [[ $arg == *$delimiter* ]];then
+#        subcommand="$(echo $arg | sed 's/^.*INPUT_.*=//' | awk -F':::' '{print $1}')"
+#        options="$(echo $arg | sed 's/^.*INPUT_.*=//' | awk -F':::' '{print $2}')"
+#        if [[ ${subcommand} == "pushconfig" ]]; then
+#            tfh ${subcommand} ${options}
+#        elif [[ ${subcommand} == "pushvars" ]]; then
+#             tfh ${subcommand} ${options}
+#        elif [[ ${subcommand} == 'pullvars' ]]; then
+#            tfh ${subcommand} ${options}
+#        else
+#            echo "tfh subcommand ${subcommand} not yet supported"
+#        fi
+#    fi
+#done
 
 # End
