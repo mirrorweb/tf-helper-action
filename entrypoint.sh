8e6e9467dc660d4b952a378c36c0440bcc7e33cf
Mark Johnson <mark.johnson@mirrorweb.com>
2020-04-18 20:28:23 +0100

    strip quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ readonly delimiter=":::"
 
 for arg in $(env | grep 'INPUT_');do
     if [[ $arg == *$delimiter* ]];then
-        subcommand="$(echo $arg | sed 's/^.*INPUT_.*=//' | awk -F':::' '{print $1}')"
-        options="$(echo $arg | sed 's/^.*INPUT_.*=//' | awk -F':::' '{print $2}')"
+        subcommand="$(echo $arg | sed 's/^.*INPUT_.*="//' | awk -F':::' '{print $1}')"
+        options="$(echo $arg | sed 's/^.*INPUT_.*="//' | awk -F':::' '{print $2}' | sed 's/"$//')"
         if [[ ${subcommand} == "pushconfig" ]]; then
             tfh ${subcommand} ${options}
         elif [[ ${subcommand} == "pushvars" ]]; then
